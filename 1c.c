4fefe47dbb83bab782c147a400d62e446a86f9e3
pkrzesniewski <44110584+pkrzesniewski@users.noreply.github.com>
2019-01-12 11:57:43 +0100

    Add files via upload

--- 1c.c
+++ 1c.c
@@ -4,18 +4,26 @@
 #include <stdio.h>
 #include <math.h>
 
+void wypisz_potegi(in upto);
+
 int main()
 {
+  
+
+  printf("Program wypisuje kolejne potęgi liczby 2, nie przekraczając liczby 2010.\n");
+  while (result < max_result)
+}
+void wypisz_potegi(int upto) {
   int power = 0;
-  int number = 2;
-  int result;
-  printf("Program wypisuje kolejne potęgi liczby 2, nie przekraczając liczby 2010 \n");
-  while (result < 2010)
-  {
-    result = pow(number, power);
-    if (result > 2010)
-      break;
-    printf("2^%d = %d \n", power, result);
+  int result = 1;
+
+
+  while (result < upto)
+    {
+    result = pow(2, power);
+    if (result > upto)
+      break;      
+    printf("2^%d = %d\n", 2, result);
     power++;
   }
 }
